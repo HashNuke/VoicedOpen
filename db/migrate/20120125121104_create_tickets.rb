65eec2c38c9d9aef7aa525cd3e79b0fb558fea3c
Akash Manohar J <akash@akash.im>
2012-01-25 17:43:34 +0530

    Adds Admin and Ticket model (and migration)
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ db/migrate/20120125121104_create_tickets.rb
@@ -0,0 +1,12 @@
+class CreateTickets < ActiveRecord::Migration
+  def change
+    create_table :tickets do |t|
+      t.text, :title
+      t.text, :message
+      t.integer :user_id
+      t.string :status, :default => "open"
+
+      t.timestamps
+    end
+  end
+end

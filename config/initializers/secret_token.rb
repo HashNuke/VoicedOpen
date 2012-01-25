a55cb3667576389ced7045bbb95997dddb7dcc4f
Akash Manohar J <akash@akash.im>
2012-01-25 17:21:42 +0530

    Creates VoicedOpen application with basic setup
    * Adds rspec, devise, cancan, factory_girl_rails
    * Sets the stylesheet syntax to sass
    * Sets the template language to HAML
    * Generates a cancan ability class
    * Deletes the public/index.html
    * Adds --format documentation to .rspec
    
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoicedOpen::Application.config.secret_token = 'bc43ba3d97e9336be9530699f1cb3865792b5971423cf56b1a6915ccce5824e002ba43b9e3ea858de998e395013426d0aed3b42cd14b8be9d6bfeeaab0bf1848'

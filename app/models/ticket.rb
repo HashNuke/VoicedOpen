b4134d796dfb824b841b4e50e4ccb10643468098
Akash Manohar J <akash@akash.im>
2012-02-03 23:19:54 +0530

    Publishes bushido events when tickets are posted
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,10 @@ class Ticket < ActiveRecord::Base
   has_many :activities,   :dependent => :destroy
   
   validate :title, :presence => true
+  validate :message, :presence => true
 
+  after_create :publish_bushido_event
+  
   def log_status_activity_by(user)
     status_action = "re-opened" if self.status == "open"
     status_action = "closed"    if self.status == "closed"
@@ -12,6 +15,24 @@ class Ticket < ActiveRecord::Base
       :action    => status_action,
       :ticket_id => self.id)
     activity.save
+  end
+
+  def publish_bushido_event
+    ticketable = self.ticketable
+    ::Bushido::Event.publish({
+        :category => :support_case,
+        :name     => :opened,
+        :data     => {
+          :author => {
+            :email => ticketable.email,
+            :first_name => ticketable.first_name,
+            :last_name  => ticketable.last_name
+          },
+          :title => self.title,
+          :body  => self.message,
+          :source => "VoicedOpen"
+        }
+      })
   end
   
 end

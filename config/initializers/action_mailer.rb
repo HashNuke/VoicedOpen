07ec9736cd715666003e75d8bcb68642aa9e2e23
Akash Manohar J <akash@akash.im>
2012-02-03 23:55:46 +0530

    Adds initializer to setup actionmailer
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => ENV['SMTP_SERVER'],
+  :port                 => ENV['SMTP_PORT'],
+  :domain               => ENV['SMTP_DOMAIN'],
+  :user_name            => ENV['SMTP_USER'],
+  :password             => ENV['SMTP_PASSWORD'],
+  :authentication       => ENV['SMTP_AUTHENTICATION'],
+  :enable_starttls_auto => ENV['SMTP_TLS']
+}

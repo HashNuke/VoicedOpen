e33a84fc947582bef78681c577c80b15fe1e6d8e
Akash Manohar J <akash@akash.im>
2012-02-03 22:54:37 +0530

    Loop on count in notifier method. Surprise count does more :)
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,7 +9,7 @@ class Activity < ActiveRecord::Base
     participants = ticket.activities.group(:actable_id, :actable_type)
     poster = ticket.ticketable
 
-    participants.each_pair do |participant_info, activities_count|
+    participants.count.each_pair do |participant_info, activities_count|
       participant_id, participant_class = participant_info
       participant = participant_class.constantize.find(participant_id)
       if participant
